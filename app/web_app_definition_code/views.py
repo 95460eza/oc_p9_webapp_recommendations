d2d44e22964c2ff1fc726cc69a9d0d4ed72a0a95
95460eza <alfredkoffisokpa@gmail.com>
2023-08-08 22:54:01 -0400

    Add port=8000 in views.py

--- app/web_app_definition_code/views.py
+++ app/web_app_definition_code/views.py
@@ -32,6 +32,6 @@ def index():
 
 
 
-#if __name__ == '__main__':
-#    app.run(host='0.0.0.0', port=8000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
 
